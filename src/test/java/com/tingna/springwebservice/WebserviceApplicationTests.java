40f8562ae40dd09ff1bbbaffef11fd680bfc6131
tingna32 <tingnana32@gmail.com>
2019-11-24 16:56:28 +0900

    v3

--- src/test/java/com/tingna/springwebservice/WebserviceApplicationTests.java
+++ src/test/java/com/tingna/springwebservice/WebserviceApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class WebserviceApplicationTests {
+public class WebserviceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 }
